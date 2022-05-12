13a5d64ca0c1466037cb3fd4fed30c26aaceaf28
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-12 19:22:59 +0530

    Job API

--- 05.Jobs API/app.js
+++ 05.Jobs API/app.js
@@ -7,6 +7,12 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 // connectDB
 const connectDB = require("./db/connect");
 
@@ -17,8 +23,18 @@ const authenticateMiddleware = require("./middleware/authentication");
 const authRouter = require("./routes/auth.js");
 const jobsRouter = require("./routes/jobs.js");
 
+app.set("trust proxy", true);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 app.use(express.json());
-// extra packages
 
 // routes
 app.get("/", (req, res) => res.send("Hello World!"));
